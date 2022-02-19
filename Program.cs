022e9ac6c8bf15639261a4d26dbc72c4657188a8
Guiorgy <Guiorgy123@gmail.com>
2022-02-19 16:45:34 +0400

    added command line arguments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
-﻿namespace CppEmbeddedHeaderGenerator
+﻿using CommandLine;
+using System;
+
+namespace CppEmbeddedHeaderGenerator
 {
     public static class Program
     {
+        public class Options
+        {
+            [Option('e', "embedded-dir", Required = true, HelpText = "Path to the directory containing the files to embedd.")]
+            public string EmbeddedDirectoryPath { get; set; } = Environment.CurrentDirectory;
+
+            [Option('i', "ignorefile", Required = false, HelpText = "Path to the ignore file (in the format of a .gitignore). By default \".embedignore\" in the same directory as the files to embedd.")]
+            public string? IgnoreFilePath { get; set; } = null;
+
+            [Option('o', "output-dir", Required = false, HelpText = "Path to the output directory. By default current directory.")]
+            public string OutputDirectoryPath { get; set; } = Environment.CurrentDirectory;
+        }
+
         static void Main(string[] args)
         {
-            EmbeddedHeaderGenerator.Generate(@"..\..\..\Embedded", null, @"..\..\..\Output");
-            EmbeddedFileExtractorGenerator.Generate(@"..\..\..\Output");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    EmbeddedHeaderGenerator.Generate(o.EmbeddedDirectoryPath, o.IgnoreFilePath, o.OutputDirectoryPath);
+                    EmbeddedFileExtractorGenerator.Generate(o.OutputDirectoryPath);
+                });
         }
     }
 }
